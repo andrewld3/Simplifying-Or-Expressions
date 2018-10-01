8d09f6f2755949229e3ed110ab38b487abaeacdb
Andrew Driscoll <adriscol@oswego.edu>
2018-10-01 17:13:58 -0400

    Or Expression complete.

--- src/MP2.clj
+++ src/MP2.clj
@@ -1,4 +1,13 @@
 (ns MP2)
 
 ;;TODO: Write Or Parser
-;;TODO: Figure out langauge
+(defn orExpression [expList]
+  (cond
+    (some true? expList) true
+    (some symbol? (rest expList)) (cond
+                                    (<= 2 (count (remove false? (rest expList)))) (expList)
+                                    (> 2 (count (remove false? (rest expList)))) (last (remove false? (rest expList)))
+                                    )
+    :else false
+    )
+    )

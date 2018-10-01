7c685a6d848b2bd2b19f5106d5c5effa1928b130
Andrew Driscoll <adriscol@oswego.edu>
2018-10-01 17:35:32 -0400

    Fixed multiple symbols

--- src/MP2.clj
+++ src/MP2.clj
@@ -5,7 +5,7 @@
   (cond
     (some true? expList) true
     (some symbol? (rest expList)) (cond
-                                    (<= 2 (count (remove false? (rest expList)))) (expList)
+                                    (<= 2 (count (remove false? (rest expList)))) (remove false? expList)
                                     (> 2 (count (remove false? (rest expList)))) (last (remove false? (rest expList)))
                                     )
     :else false
